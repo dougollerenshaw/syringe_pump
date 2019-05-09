9e1bda9005df13f5a1ac8aa64f30b9f80c1f1a87
dougollerenshaw <d.ollerenshaw@gmail.com>
2019-05-09 20:28:37 +0100

    update server

--- stepper_server.py
+++ stepper_server.py
@@ -18,8 +18,9 @@ class StepperServer(Stepper):
         print('syringe = {}'.format(syringe))
 
 
-daemon = Pyro4.Daemon("192.168.0.46")                # make a Pyro daemon
+daemon = Pyro4.Daemon("10.128.50.194")                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
+print("nameserver = {}".format(ns))
 uri = daemon.register(StepperServer)   # register the stepper server as a Pyro object
 ns.register("stepper.server", uri)   # register the object with a name in the name server
 
